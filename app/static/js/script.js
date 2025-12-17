7d912b7af2eca277806065020068df87d6d3bfeb
Naomi Kurian <nkurian60@stuy.edu>
2025-12-17 11:21:22 -0500

    cw

---
+++ app/static/js/script.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('hatch').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const element = document.getElementById('bttm');
+    element.classList.add('hatching');
+});
